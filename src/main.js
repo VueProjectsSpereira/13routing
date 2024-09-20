0df002a55465106fe2d014fbab9b1707a5941d2b
Saul Pereira <saul_pereira@hotmail.com>
2024-09-20 12:22:20 -0600

    180B. Redirecting & "Catch All" Routes
    180B. Redirecting & "Catch All" Routes

--- src/main.js
+++ src/main.js
@@ -9,10 +9,10 @@ import TeamMembers from './components/teams/TeamMembers.vue';
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', redirect: '/teams' },
-    { path: '/teams', component: TeamsList },
+    { path: '/teams', component: TeamsList, alias: '/' },
     { path: '/users', component: UsersList },
     { path: '/teams/:teamId', component: TeamMembers, props: true },
+    { path: '/:notFound(.*)', redirect: '/teams' },
   ],
   linkActiveClass: 'active',
 });
